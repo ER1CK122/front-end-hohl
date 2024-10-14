010a9bb0557f3d0eeffc1eca815606004342ee05
ER1CK122 <erckgames53@gmail.com>
2024-10-14 15:40:49 -0300

    Adicionado tailwind e daisyUI para melhor design da tela. Feito conexão com o servidor. issueId#02

---
+++ public/src/app.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"> 
+   async function api() {
+    const apiBun = await useFetch('/home', {
+      method: 'GET',
+      baseURL: "http://localhost:3333/"
+    });
+
+    let text = JSON.stringify(apiBun.data.value);
+    text = JSON.parse(text).data;
+    
+    console.log(text);
+ }
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center align-middle">
+    <button class="btn" @click="api">clique aqui</button>
+  </div>
+</template>
